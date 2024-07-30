a8ab7266d96dd096fa50751a1a6b01be9e184b98
David Silvente <dsilvente2@gmail.com>
2024-07-30 10:54:28 +0200

    Pagina mis urls, funciones de copiar y eliminar

--- src/app/urls/urls.component.ts
+++ src/app/urls/urls.component.ts
@@ -9,11 +9,32 @@ import { UrlShortenerService } from '../url-shortener.service';
 export class UrlsComponent implements OnInit{
 
   savedUrls: { shortUrl: string, originalUrl: string }[] = [];
+  dropdownUrl: string | null = null;
 
   constructor(private urlShortenerService: UrlShortenerService) {}
 
   ngOnInit() {
     this.savedUrls = this.urlShortenerService.getUrls();
+  }
+
+  toggleDropdown(url: { shortUrl: string }) {
+    this.dropdownUrl = this.dropdownUrl === url.shortUrl ? null : url.shortUrl;
+  }
+
+  copyUrl(url: { shortUrl: string }) {
+    const fullUrl = `http://localhost:3000/${url.shortUrl}`;
+    navigator.clipboard.writeText(fullUrl).then(() => {
+      console.log('URL copiada al portapapeles');
+      this.dropdownUrl = null;
+    }).catch(err => {
+      console.error('Error al copiar la URL: ', err);
+    });
+  }
+
+  deleteUrl(url: { shortUrl: string }) {
+    this.savedUrls = this.savedUrls.filter(u => u.shortUrl !== url.shortUrl);
+    localStorage.setItem('shortUrls', JSON.stringify(this.savedUrls));
+    this.dropdownUrl = null;
   }
 
 }
