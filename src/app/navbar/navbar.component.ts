8b9d119554248fe692e1eb45bdc4a3c6d303e542
David Silvente <dsilvente2@gmail.com>
2024-08-17 16:54:18 +0200

    Footer responsive, cambios en estilos

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavigationService } from '../navigation.service';
 
@@ -8,11 +8,27 @@ import { NavigationService } from '../navigation.service';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  menuActive: boolean = false;
 
-  constructor(private route: ActivatedRoute, private navigationService: NavigationService) {}
+  constructor(private route: ActivatedRoute, private navigationService: NavigationService, private elementRef: ElementRef, private renderer: Renderer2) {}
 
   goToPage(page: string) {
     this.navigationService.navigateToRoute(page);
+  }
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+
+    if (this.menuActive) {
+      document.body.classList.add('menu-active');
+    } else {
+      document.body.classList.remove('menu-active');
+    }
+  }
+
+  closeMenu() {
+    this.menuActive = false;
+    this.renderer.setStyle(document.body, 'overflow', 'auto');
   }
 
 }
